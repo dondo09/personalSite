07bc0ca69082422eb957a3a27dc97d5cbf849e3a
Christian Arredondo <j.christian.arredondo@gmail.com>
2017-04-26 09:58:07 -0500

    Initial commit of files

---
+++ client/index.js
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Christian Arredondo</title>
+  <meta name="description" content="I am an Austin-based software engineer specializing in fullstack JavaScript web development.">
+  <meta name="author" content="Tyler Holzer">
+  <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/public/styles/stylesheet.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,300" rel="stylesheet">
+</head>
+
+<body>
+  <!-- TITLE -->
+  <div class="container-fluid">
+    <div class="row header">
+      <h1 class="heading">J.  &nbsp;CHRISTIAN &nbsp;ARREDONDO</h1>
+    </div>
+
+  <!-- DESCRIPTION -->
+    <div class="bio-container">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-3">
+          <img class="author" src="https://avatars2.githubusercontent.com/u/22177431?v=3" />
+        </div>
+
+        <div class="col-md-5">
+          <h4 class="lead">Hello! I am a fullstack Javascript engineer based in Austin, TX. I have developed applications using several leading frameworks and technologies, among them React, Redux, AngularJS, jQuery, Node.js, Express, SQL Databases and MongoDB. I am an avid outdoors enthusiast and play for the nationally ranked Australian Rules football team, the Austin Crows. You can find some of my most recent applications down below along with the best ways to reach me.
+          </h4>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+    </div>
+
+  <!-- APPLICATIONS -->
+    <div class="row">
+      <h2 class="b-head">Recent Applications</h2>
+    </div>
+    <!-- SCREENSHOTS -->
+    <div class="row">
+      <div class="col-md-2"></div>
+        <div class="col-md-3">
+          <h3 class="app-title">My Sentry</h3>
+            <a href="http://hackalope.io">
+              <img class="app-photo" src="/public/assets/hackalope.png" />
+            </a>
+          <br />
+          <p class="app-desc">My Sentry is an android application created for people that work in solo careers. It allows them to create groups and events to send out notifications if they are ever in danger. My Sentry was built using React Native with Redux, Node.js, Express and MySQL. It also utilized Firebase Messaging for notifications and Google Places API for location services.</p>
+          <!-- Link to Repo -->
+        </div>
+        <div class="col-md-3">
+          <h3 class="app-title">CardCast</h3>
+            <a href="http://cardcast.tylerholzer.com">
+              <img class="app-photo" src="/public/assets/cardcast.png" />
+            </a>
+          </br />
+          <p class="app-desc">Cardcast is a collaborative presentation platform that utilizes the Google ChromeCast. Chromecast allows users to create and save slide decks with markdown language. They can project these slides to any deviced connected to ChromeCast. It was built using AngularJS, MongoDB, Node.js, and Express.
+        </div>
+        <div class="col-md-3">
+          <h3 class="app-title">Echoes</h3>
+            <a href="http://echoes.tylerholzer.com">
+              <img class="app-photo" src="/public/assets/echoes.png" />
+            </a>
+          <br />
+          <p class="app-desc">Echoes is a living listening history. With Echoes, it's easy to rate,remember, and relive the albums you've enjoyed. This app was inspired by a listening journal that one of our team members has been keeping for the past seven years. It was built on React, PostgreSQL, Node.js, and Express. Echoes was designed using Bootstrap.
+        </div>
+      <div class="col-md-1"></div>
+    </div>
+
+    <!-- RESUME AND CONTACT INFORMATION -->
+    <div class="row">
+      <h2 class="b-head">Contact Me</h2>
+      <br />
+      <h4 class="c-head">Find me on LinkedIn and Github, view my current resume, or send me an email.</h4>
+    </div>
+
+    <div class="row links">
+      <div class="col-md-4"></div>
+      <div class="col-md-1">
+        <!-- LinkedIn -->
+        <a href="http://linkedin.com/in/jchristianarredondo">
+          <img src="public/assets/linkedin.png" />
+        </a>
+      </div>
+      <div class="col-md-1">
+        <!-- Github -->
+        <a href="http://github.com/dondo09">
+          <img src="public/assets/github.png" />
+        </a>
+      </div>
+      <div class="col-md-1">
+        <!-- Resume -->
+        <a href="/public/assets/resume.pdf">
+          <img src="public/assets/resume.png" />
+        </a>
+      </div>
+      <div class="col-md-1">
+        <!-- Email -->
+        <a href="mailto:j.christian.arredondo@gmail.com">
+          <img src="public/assets/gmail.png" />
+        </a>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+
+  </div>
+</body>
+</html>
---
+++ server/server.js
@@ -0,0 +1,26 @@
+// DEPENDENCIES
+const express = require('express');
+const path = require ('path');
+
+const app = express();
+
+// STATIC FILES
+app.use('/public', express.static(path.join(__dirname, '../client')));
+app.use('/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap')));
+
+// SERVE INDEX PAGE
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+// CATCH 404 ERRORS
+app.use((req, res, next) => {
+  const err = new Error('Sorry--I couldn\'t find that!');
+  err.status = 404;
+  next(err);
+});
+
+// LISTEN
+app.listen(8081, () => {
+  console.log(`Server is listening on port 8081.`)
+});
